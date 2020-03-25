d5fb47120cbf5ecd78973826dbe096e663d2c461
Mohamed Ali <mohamedkosol@gmail.com>
2020-03-25 12:27:52 -0500

    make it better

--- src/Components/About.js
+++ src/Components/About.js
@@ -19,7 +19,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="" />
+            <img className="profile-pic"  src={profilepic} alt="mo-2.JPG" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
